f1927a90ebdf72b2b6e886796290249ba3e1c6cc
SamGombs20 <gombajosh18@gmail.com>
2026-01-14 12:41:04 +0300

    Update:Banner development

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -5,6 +5,7 @@ export const Banner = () => {
             <div className={style.main}>
                 <div className={style.intro}>
                     <p className={style.intro_name}>Hey There, <br />I'm Joshua</p>
+                    <p className={style.intro_desc}>I build mobile apps, web applications, and machine learning models that solve real problems. I love what I do.  </p>
                 </div>
             </div>
         </div>
