3a40a4e200f9b3625ff2e1ab7e7e8f95faed41f4
SamGombs20 <gombajosh18@gmail.com>
2026-01-15 10:59:53 +0300

    Update:Banner development

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,35 +1,37 @@
 'use client'
 import { useState } from "react"
 import style from "../styles/navbar.module.css"
-const Navbar =()=>{
-    const [open,setOpen] = useState(false)
+const Navbar = () => {
+    const [open, setOpen] = useState(false)
 
-    const handleClick = ()=>{
+    const handleClick = () => {
         setOpen(!open)
     }
-    return(
-        <div className={style.navbar}>
-            <div>
-                <p className={style.nav_title}>Joshua</p>
-            </div>
-            <div className={style.nav_items}>
-                <ul className={open ? `${style.menu_items} ${style.open}`:style.menu_items}>
-                    <li>
-                        <a href="#" className={style.nav_link}>SERVICES</a>
-                    </li>
-                    <li>
-                        <a href="#" className={style.nav_link}>WORKS</a>
-                    </li>
-                    <li>
-                        <a href="#" className={style.nav_link}>EXPERIENCE</a>
-                    </li>
-                </ul>
-            </div>
-            <div className={style.nav_contact}>
-                <p>+254 115 185 192</p>
-            </div>
-            <div className={style.nav_toggle} onClick={handleClick}>
-                <i className={open? `fas fa-times`:`fas fa-bars`}></i>
+    return (
+        <div className={style.main}>
+            <div className={style.navbar}>
+                <div>
+                    <p className={style.nav_title}>Joshua</p>
+                </div>
+                <div className={style.nav_items}>
+                    <ul className={open ? `${style.menu_items} ${style.open}` : style.menu_items}>
+                        <li>
+                            <a href="#" className={style.nav_link}>SERVICES</a>
+                        </li>
+                        <li>
+                            <a href="#" className={style.nav_link}>WORKS</a>
+                        </li>
+                        <li>
+                            <a href="#" className={style.nav_link}>EXPERIENCE</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className={style.nav_contact}>
+                    <p>+254 115 185 192</p>
+                </div>
+                <div className={style.nav_toggle} onClick={handleClick}>
+                    <i className={open ? `fas fa-times` : `fas fa-bars`}></i>
+                </div>
             </div>
         </div>
     )
