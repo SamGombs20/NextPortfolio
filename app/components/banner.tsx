2e45d668e04c279d90b649f6711fb4ab101f80b6
SamGombs20 <gombajosh18@gmail.com>
2026-01-15 11:59:43 +0300

    Update:Banner development

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -7,16 +7,19 @@ export const Banner = () => {
                     <p className={style.intro_name}>Hey There, <br />I'm Joshua</p>
                     <p className={style.intro_desc}>I build mobile apps, web applications, and machine learning models that solve real problems. I love what I do.  </p>
                 </div>
-                <div className={style.email_container}>
+                <div className={style.banner_img}>
+
+                </div>
+                <div className={style.additional_info}>
+                    <div className={style.email_container}>
                     <p>gombajosh18@gmail.com</p>
                 </div>
                 <div className={style.experience_container}>
                     <p className={style.years}>2</p>
                     <p>YEARS <br />EXPERIENCE</p>
                 </div>
-                <div className={style.banner_img}>
-
                 </div>
+                
             </div>
         </div>
     );
