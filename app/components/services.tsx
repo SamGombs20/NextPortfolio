c0c8e76e1a674167086d3e3b9a6170ef3b1aff66
SamGombs20 <gombajosh18@gmail.com>
2026-01-15 13:54:12 +0300

    Update:NavBar responsivenes done

---
+++ app/components/services.tsx
@@ -0,0 +1,8 @@
+import style from "../styles/services.module.css"
+export const Services =()=>{
+    return(
+        <div className={style.main}>
+            
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Banner } from './components/banner';
+import { Services } from './components/services';
 import style from './styles/home.module.css'
 export default function Home() {
   return (
     <div>
       <Banner/>
+      <Services/>
     </div>
   );
 }
