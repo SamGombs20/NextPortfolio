c2453f46df6dcbeae9a8d98752d71b63015435a6
SamGombs20 <gombajosh18@gmail.com>
2026-01-16 13:48:00 +0300

    Update:Services section development

---
+++ app/components/experience.tsx
@@ -0,0 +1,10 @@
+import style from "../styles/experience.module.css"
+export const Experience =()=>{
+    return (
+        <div className={style.main}>
+            <div className={style.experiences_container}>
+                <div></div>
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Banner } from './components/banner';
+import { Experience } from './components/experience';
 import { Services } from './components/services';
 import style from './styles/home.module.css'
 export default function Home() {
   return (
     <div>
       {/* <Banner/> */}
-      <Services/>
+      {/* <Services/> */}
+      <Experience/>
     </div>
   );
 }
