b5c524093726b4bc40c39f755833bd70baded9d3
SamGombs20 <gombajosh18@gmail.com>
2026-01-16 15:46:32 +0300

    Update:Services section development

--- app/components/experience.tsx
+++ app/components/experience.tsx
@@ -4,7 +4,13 @@ export const Experience =()=>{
         <div className={style.main}>
             <div className={style.experience_container}>
                 <p className={style.title}>Education & Experience</p>
-                <div></div>
+                <div className={style.education}>
+                    <p className={style.year}>2020-2024</p>
+                    <div>
+                        <p className={style.course}>BSc. Computer Science</p>
+                        <p>JKUAT</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
