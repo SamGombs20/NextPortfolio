b99250cf29d6be9fc04121f67112f6ea1562972d
SamGombs20 <gombajosh18@gmail.com>
2026-01-20 15:17:19 +0300

    Update:Projects section development

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import style from "../styles/projects.module.css";
 export const Projects = () => {
   return (
@@ -5,12 +6,43 @@ export const Projects = () => {
       <div className={style.projects_container}>
         <p className={`${style.title} title-text`}>My Latest Projects</p>
         <div className={style.projects_list}>
-            <div>
+          <div className={style.project_container}>
+            <Image
+              src="/obesi_web.png"
+              alt=""
+              width={300}
+              height={290}
+              className={style.project_img}
+            />
+            <div className={style.project_details}>
                 
+                <p className={style.project_category}>Web application</p>
+                <p className={style.project_title}>ObesiScan</p>
             </div>
+          </div>
+          <div className={style.project_container}>
+            <Image
+              src="/course_web.png"
+              alt=""
+              width={300}
+              height={290}
+              className={style.project_img}
+            />
+            <div>
+              <p></p>
+            </div>
+          </div>
+          <div className={style.project_container}>
+            <Image
+              src="/obesi_mobile.png"
+              alt=""
+              width={300}
+              height={290}
+              className={style.project_img}
+            />
+          </div>
         </div>
       </div>
-
     </div>
   );
 };
