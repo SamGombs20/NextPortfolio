7bcb3ca9fe731ff0cf9c4b2ea73d8f88d134e914
SamGombs20 <gombajosh18@gmail.com>
2026-01-20 12:46:08 +0300

    Update:Experience module development

--- app/components/experience.tsx
+++ app/components/experience.tsx
@@ -1,17 +1,23 @@
-import style from "../styles/experience.module.css"
-export const Experience =()=>{
-    return (
-        <div className={style.main}>
-            <div className={style.experience_container}>
-                <p className={style.title}>Education & Experience</p>
-                <div className={style.education}>
-                    <p className={style.year}>2020-2024</p>
-                    <div>
-                        <p className={style.course}>BSc. Computer Science</p>
-                        <p>JKUAT</p>
-                    </div>
-                </div>
+import style from "../styles/experience.module.css";
+export const Experience = () => {
+  return (
+    <div className={style.main}>
+      <div className={style.experience_container}>
+        <p className={style.title}>Education & Experience</p>
+        <div>
+          <div className={style.education}>
+            <p className={style.year}>2020-2024</p>
+            <div>
+              <p className={style.course}>BSc. Computer Science</p>
+              <p>JKUAT</p>
             </div>
+            <div>
+                <p className={style.year}>Sep 2024 - Present</p>
+                <p>Software Engineer (Intern)</p>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
