aa72d07b3c7b3fcbcef5a4d629da636bb2b50a84
SamGombs20 <gombajosh18@gmail.com>
2026-01-20 13:08:07 +0300

    Update:Experience module development

--- app/components/experience.tsx
+++ app/components/experience.tsx
@@ -4,18 +4,23 @@ export const Experience = () => {
     <div className={style.main}>
       <div className={style.experience_container}>
         <p className={style.title}>Education & Experience</p>
-        <div>
+        <div className={style.sub_container}>
           <div className={style.education}>
             <p className={style.year}>2020-2024</p>
-            <div>
-              <p className={style.course}>BSc. Computer Science</p>
-              <p>JKUAT</p>
-            </div>
-            <div>
-                <p className={style.year}>Sep 2024 - Present</p>
-                <p>Software Engineer (Intern)</p>
-            </div>
+
+            <p className={style.main_text}>BSc. Computer Science</p>
+            <p>JKUAT</p>
           </div>
+          <div className={style.experience}>
+              <p className={style.year}>June 2023 - August 2023</p>
+              <p className={style.main_text}>Software Engineer (Attachee)</p>
+              <p>Mutall Investment Company Ltd.</p>
+            </div>
+          <div className={style.experience}>
+              <p className={style.year}>Sep 2024 - Present</p>
+              <p className={style.main_text}>Software Engineer (Intern)</p>
+              <p>Artlly Group Ltd.</p>
+            </div>
         </div>
       </div>
     </div>
