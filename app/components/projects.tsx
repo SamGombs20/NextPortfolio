7eb69144b01dde7d6fa8c4b31a2b7034a8f9c539
SamGombs20 <gombajosh18@gmail.com>
2026-01-20 13:51:46 +0300

    Update:Experience module development done

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,7 +1,16 @@
-export const Projects = ()=>{
-    return(
-        <div className="">
-
+import style from "../styles/projects.module.css";
+export const Projects = () => {
+  return (
+    <div className={style.main}>
+      <div className={style.projects_container}>
+        <p className={`${style.title} title-text`}>My Latest Projects</p>
+        <div className={style.projects_list}>
+            <div>
+                
+            </div>
         </div>
-    );
-}
+      </div>
+
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Banner } from './components/banner';
 import { Experience } from './components/experience';
+import { Projects } from './components/projects';
 import { Services } from './components/services';
 import style from './styles/home.module.css'
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <Banner/>
       <Services/>
+      <Projects/>
       <Experience/>
     </div>
   );
