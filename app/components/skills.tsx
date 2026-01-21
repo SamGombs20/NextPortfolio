1e9bfa82e2d505884179be30a8079d189068d20e
SamGombs20 <gombajosh18@gmail.com>
2026-01-21 13:37:27 +0300

    Update:Skills section development

--- app/components/skills.tsx
+++ app/components/skills.tsx
@@ -4,6 +4,11 @@ export const Skills =()=>{
         <div className={`main ${style.main}`}>
             <div className={style.container}>
                 <p className={`title-text ${style.title}`}>Tech Stack</p>
+                <div className={style.skills_lists}>
+                    <div>
+                        <p>Front</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
