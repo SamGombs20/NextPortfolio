a627ff9a604317eaaba4655584e29c6b651e0374
SamGombs20 <gombajosh18@gmail.com>
2026-01-21 15:36:20 +0300

    Update:Footer development

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,15 +1,27 @@
+'use client'
 import Link from "next/link";
 import style from "../styles/footer.module.css";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa6";
 import { FaCloudDownloadAlt } from "react-icons/fa";
 export const Footer = () => {
+    const downloadResume = () => {
+        const fileId = "18_6JzMMAM9wH70GSaHqsDuy6npFkvD1V"
+        const directDownloadUrl = `https://drive.google.com/uc?export=download&id=${fileId}`
+
+        const link = document.createElement('a');
+        link.href = directDownloadUrl;
+        link.download = 'Josh_resume.pdf'
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
+    }
   return (
     <div className={`main`}>
       <div className={style.footer_container}>
         <div className={style.reserved}>
           <p>&copy; 2026 JOSH, ALL RIGHTS RESERVED</p>
         </div>
-        <div className={style.resume}>
+        <div className={style.resume} onClick={downloadResume}>
             <FaCloudDownloadAlt/>
             <p>My Resume</p>
         </div>
