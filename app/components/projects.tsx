1c670497fe5df469a2ee6026b906d5d51f2124b9
SamGombs20 <gombajosh18@gmail.com>
2026-01-22 10:22:13 +0300

    Update:More projects

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -8,7 +8,7 @@ export const Projects = () => {
   return (
     <div className={style.main} id="works">
       <div className={style.projects_container} >
-        <p className={`${style.title} title-text`}>My Latest Projects</p>
+        <p className={`${style.title} title-text`}>My Projects</p>
         <div className={style.projects_list}>
           {/* <div className={style.project_container}>
             <Image
@@ -72,6 +72,29 @@ export const Projects = () => {
               <p className={style.project_title}>Course Tracker</p>
             </div>
           </div>
+          <div className={style.project_container} onClick={()=> handleClick("https://github.com/SamGombs20/TaxiPricingModel/blob/main/notebooks/taxiPricingPrediction.ipynb")}>
+           <div className={style.image_wrapper}>
+              <Image
+                src="/taxi.webp"
+                alt="Course Tracker project screenshot"
+                width={320}
+                height={290}
+                className={style.project_img}
+              />
+
+             
+              <div className={style.overlay}>
+                <div className={style.overlay_content}>
+                  
+                  <span className={style.view_btn}>See Project →</span>
+                </div>
+              </div>
+            </div>
+            <div className={style.project_details}>
+              <p className={style.project_category}>ML Model</p>
+              <p className={style.project_title}>Taxi Fare Prediction</p>
+            </div>
+          </div>
           <div className={style.project_container} onClick={()=> handleClick("https://github.com/SamGombs20/ObesityApp/blob/master/ObesiScan.mp4")}>
            <div className={style.image_wrapper}>
               <Image
@@ -93,6 +116,29 @@ export const Projects = () => {
             <div className={style.project_details}>
               <p className={style.project_category}>Mobile application</p>
               <p className={style.project_title}>ObesiScan</p>
+            </div>
+          </div>
+          <div className={style.project_container} onClick={()=> handleClick("https://github.com/SamGombs20/StockPrediction/blob/main/Classification_stock.ipynb")}>
+           <div className={style.image_wrapper}>
+              <Image
+                src="/stock.webp"
+                alt="Stock prediction project screenshot"
+                width={320}
+                height={290}
+                className={style.project_img}
+              />
+
+             
+              <div className={style.overlay}>
+                <div className={style.overlay_content}>
+                  
+                  <span className={style.view_btn}>See Project →</span>
+                </div>
+              </div>
+            </div>
+            <div className={style.project_details}>
+              <p className={style.project_category}>ML Model</p>
+              <p className={style.project_title}>Stock Price Prediction</p>
             </div>
           </div>
         </div>
