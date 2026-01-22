f6aa74cb04a6480a5663632c4d37fff0e6719bbe
SamGombs20 <gombajosh18@gmail.com>
2026-01-22 10:26:32 +0300

    Update:Banner update

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -7,7 +7,7 @@ export const Banner = () => {
                 <div className={style.intro}>
                     <p className={style.intro_name}>Hey There, <br />I'm Joshua</p>
                     <p className={style.intro_desc}>
-                        <strong>Full-Stack Developer</strong> | React • Next.js • FastAPI • Android (Compose) 
+                        <strong>Full-Stack Developer</strong> | React • Next.js • FastAPI • Android • ML 
                         <br /> <br />
                         
                         I build mobile apps, web applications, and machine learning models that solve real problems. I love what I do.  </p>
